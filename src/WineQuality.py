9059a0180cc2fc56ac66e86e4e9278ef691f32d1
Lucy Lu <lucy.y.lu@cnsdc01.pwc.com>
2018-09-12 17:46:23 +0800

    adjust style

--- src/WineQuality.py
+++ src/WineQuality.py
@@ -11,11 +11,6 @@ import math
 
 #step 2: Get data from the file and encode the labels using LabelEncoder class.
 Data= pd.read_table("/Users/llu159/Desktop/small_code/classification/Wine_Quality_Data_Set/winequality-red.csv", sep= None, engine= "python")
-#cols= ["age","balance","day","duration","campaign","pdays","previous"]
-#data_encode= Data.drop(cols, axis= 1)
-#data_encode= data_encode.apply(LabelEncoder().fit_transform)
-#data_rest= Data[cols]
-#Data= pd.concat([data_rest,data_encode], axis= 1)
 
 #step 3: split the data into training and testing set.
 data_train, data_test= train_test_split(Data, test_size= 0.2, random_state= 42)
@@ -36,24 +31,14 @@ X_train= scaler.transform(X_train)
 X_test= scaler.transform(X_test)
 
 
-#step 5: Determine [latex]\sigma[/latex]
-# max= 0;
-# for i in range(K_clust):
-# 	for j in range(column):
-# 		d= numpy.linalg.norm(Cent[i]-Cent[j])
-# 		if (max<d):
-# 			max= d
-# d= max
-# sigma= d/math.sqrt(2*K_clust)
-
-#step 6: Determine centers of the neurons using KMeans.
+#step 5: Determine centers of the neurons using KMeans.
 K_cent= 8
 km= KMeans(n_clusters= K_cent, max_iter= 100)
 km.fit(X_train)
 cent= km.cluster_centers_
 
 
-#step 7: Determine the value of [latex]\sigma[/latex]
+#step 6: Determine the value of [latex]\sigma[/latex]
 max=0
 for i in range(K_cent):
 	for j in range(K_cent):
@@ -64,7 +49,7 @@ d= max
 sigma= d/math.sqrt(2*K_cent)
 print('The sigma is:',sigma)
 
-#step 8: Set up matrix G, the outpu of hidden layer neuron.
+#step 7: Set up matrix G, the outpu of hidden layer neuron.
 shape= X_train.shape
 row= shape[0]
 print('The row is:',row)
@@ -77,14 +62,14 @@ for i in range(row):
         dist= numpy.linalg.norm(X_train[i]-cent[j])
         G[i][j]= math.exp(-norm(dist)/(2*math.pow(sigma,2)))
 
-#step 9: Find weight matrix W to train the network.
+#step 8: Find weight matrix W to train the network.
 GTG= numpy.dot(G.T,G)
 GTG_inv= numpy.linalg.inv(GTG)
 fac= numpy.dot(GTG_inv,G.T)
 W= numpy.dot(fac,Y_train)
 
 
-#step 10: Set up matrix G for the test set.
+#step 9: Set up matrix G for the test set.
 row = X_test.shape[0]
 column = K_cent
 G_test = numpy.empty((row, column), dtype=float)
@@ -93,7 +78,7 @@ for i in range(row):
         dist = numpy.linalg.norm(X_test[i] - cent[j])
         G_test[i][j] = math.exp(-norm(dist) / (2*math.pow(sigma, 2)))
 
-#step 11: Analyze the accuracy of prediction on test set
+#step 10: Analyze the accuracy of prediction on test set
 prediction= numpy.dot(G_test,W)
 prediction= 0.5*(numpy.sign(prediction-0.5)+1)
 print(prediction)
